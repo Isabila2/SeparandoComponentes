d7c155d2eaf7328ab11fdf461d66e32b99d31a74
Isabella <125681801+Isabila2@users.noreply.github.com>
2024-03-08 12:52:21 -0300

    children ainda

--- src/components/LoginImageBackground.js
+++ src/components/LoginImageBackground.js
@@ -1,30 +1,11 @@
 import { View, ImageBackground } from "react-native";
-{
-  /* Chamando LoginText */
-}
-import LoginText from "../components/LoginText";
-
-{
-  /* Chamando LoginInput */
-}
-import LoginInput from "../components/LoginInput";
-
-{
-  /* Chamando Button */
-}
-import LoginButton from "../components/LoginButton";
-
-{
-  /* Chamando Imagem */
-}
-import LoginImage from "../components/LoginImage";
 
 {
   /* Chamando Styles */
 }
 import styles from "../styles/StyleSheet";
 
-export default function LoginBackground({children}) {
+export default function LoginBackground({ children }) {
   return (
     <View style={styles.container}>
       {/* Imagem de fundo */}
